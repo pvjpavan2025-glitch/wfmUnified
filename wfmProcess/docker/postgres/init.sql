3263d8ce0e1f3351bc37b2f74ef572119565708c
PVJ Pavan Kumar <pavan@tsarolabs.com>
2025-08-28 16:47:37 +0530

    Commented out version in docker-compose and fixied init.sql between multiple docker builds

--- wfmProcess/docker/postgres/init.sql
+++ wfmProcess/docker/postgres/init.sql
@@ -1,8 +1,15 @@
--- Create the workflow database
-CREATE DATABASE workflow;
+-- Create the workflow database (if not exists)
+SELECT 'CREATE DATABASE workflow'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'workflow')\gexec
 
--- Create the workflow_user with password
-CREATE USER workflow_user WITH PASSWORD 'workflow_pass';
+-- Create the workflow_user with password (if not exists)
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'workflow_user') THEN
+      CREATE USER workflow_user WITH PASSWORD 'workflow_pass';
+   END IF;
+END
+$$;
 
 -- Grant all privileges on the workflow database to workflow_user
 GRANT ALL PRIVILEGES ON DATABASE workflow TO workflow_user;
@@ -17,4 +24,4 @@ GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO workflow_user;
 
 -- Set default privileges for future tables
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO workflow_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES IN SCHEMA public TO workflow_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO workflow_user;

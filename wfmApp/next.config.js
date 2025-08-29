a06249f10ee7cb326874fc98ee2f800bfbab9c9b
PVJ Pavan Kumar <pavan@tsarolabs.com>
2025-08-29 17:31:59 +0530

    Fixed LoginPage and supress errors

--- wfmApp/next.config.js
+++ wfmApp/next.config.js
@@ -9,9 +9,8 @@ const nextConfig = {
       ? 'https://wfm-unified-apim.azure-api.net/process/v1' 
       : process.env.NEXT_PUBLIC_PROCESS_ENGINE_URL || 'http://localhost:8100'
   },
-  experimental: {
-    serverComponentsExternalPackages: ['bpmn-js']
-  }
+  // Next.js 15+: use top-level serverExternalPackages instead of deprecated experimental.serverComponentsExternalPackages
+  serverExternalPackages: ['bpmn-js']
 }
 
 module.exports = nextConfig

ff434b9df9a2eed9e0b5f982487ef810b3759d86
“Nalini <nalini@tsarolabs.com>
2025-08-30 23:03:08 -0500

    css error commented

--- wfmApp/components/modelling/BpmnModeler.tsx
+++ wfmApp/components/modelling/BpmnModeler.tsx
@@ -11,7 +11,7 @@ import MinimapModule from 'diagram-js-minimap';
 
 import 'bpmn-js/dist/assets/diagram-js.css';
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
-import 'bpmn-js-properties-panel/dist/assets/properties-panel.css';
+//import 'bpmn-js-properties-panel/dist/assets/properties-panel.css'; // CSS file not found in current version
 import 'diagram-js-minimap/assets/diagram-js-minimap.css';
 import './BpmnModeler.css';
 

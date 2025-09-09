7648085943be334ebbdb61130f620e98f1af1631
PVJ Pavan Kumar <pavan@tsarolabs.com>
2025-09-09 18:57:29 +0530

    Added properties panel imports

--- wfmApp/components/modelling/BpmnModeler.tsx
+++ wfmApp/components/modelling/BpmnModeler.tsx
@@ -14,8 +14,6 @@ import type { BpmnElement } from '@/types/global';
 
 import 'bpmn-js/dist/assets/diagram-js.css';
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
-import 'bpmn-js-properties-panel/dist/assets/properties-panel.css';
-import 'bpmn-js-properties-panel/dist/assets/element-templates.css';
 import '@bpmn-io/properties-panel/assets/properties-panel.css';
 import 'diagram-js-minimap/assets/diagram-js-minimap.css';
 import './BpmnModeler.css';
